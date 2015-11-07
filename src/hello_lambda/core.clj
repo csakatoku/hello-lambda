76f6e10d0287b3facd0c0fd47f0293db1a2d0e82
Chihiro SAKATOKU <csakatoku@gmail.com>
2015-11-07 08:16:19 +0000

    Implement RequestStreamHandler

--- src/hello_lambda/core.clj
+++ src/hello_lambda/core.clj
@@ -1,6 +1,16 @@
 (ns hello_lambda.core
   (:gen-class
-   :methods [^:static [handler [String] String]]))
+   :implements [com.amazonaws.services.lambda.runtime.RequestStreamHandler])
+  (:require [clojure.data.json :as json]
+            [clojure.string :as s]
+            [clojure.java.io :as io]))
 
-(defn -handler [s]
-  (str "Hello " s "!"))
+(defn hello [params]
+  {:message (str "Hello " (:name params))})
+
+(defn -handleRequest [this is os context]
+  (let [w (io/writer os)]
+    (-> (json/read (io/reader is) :key-fn keyword)
+        (hello)
+        (json/write w))
+    (.flush w)))
